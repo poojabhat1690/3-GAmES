0a2f78be7be1a5f5d3729722c9267ffe05933e16
pooja.bhat <poojabhat1690@gmail.com>
2019-11-24 19:47:38 +0100

    removed ml singularity

--- bin/getDependencies.sh
+++ bin/getDependencies.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-module load singularity/3.2.1
+#module load singularity/3.2.1
 #### pulling dependencies and slamdunk
+command -v singularity >/dev/null 2>&1 || { echo >&2 "3' GAmES requires singularity version > 3.0, please load this and try again."; exit 1; }
+
+
 echo "pulling dependencies for 3'GAmES"
 singularity pull shub://poojabhat1690/dependencies
 
